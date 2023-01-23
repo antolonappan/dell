ca7eb6c975f251f929c6f10e19f41ad93c9b62e1
Anto I Lonappan <antoidicherian@gmail.com>
2023-01-23 07:31:53 -0800

    recplot dev

--- plotstat.py
+++ plotstat.py
@@ -18,7 +18,7 @@ class simStat:
         self.fg2 = fg2
     
     def plot_fg1(self):
-        fname = f'../Data/paper/simFG1.pkl'
+        fname = '../Data/paper/simFG1.pkl'
         if os.path.isfile(fname):
             data = pl.load(open(fname,'rb'))
             print('Data Loaded from file')
@@ -51,21 +51,34 @@ class simStat:
         plt.xlim(2,800)
         plt.ylim(1e-7,10)
         plt.legend(ncol=3,fontsize=15)
-        plt.xlabel("$\ell$",fontsize=20)
-        plt.ylabel("$\frac{1}{b_\ell^2}\left(C_\ell + C_\ell^{FG\;res} + N_\ell \right)$ [$\mu K^2$]",fontsize=20)
+        plt.xlabel(r"$\ell$",fontsize=20)
+        plt.ylabel("$\\frac{1}{b_\ell^2}\\left(C_\ell + C_\ell^{FG\;res} + N_\ell \\right)$ [$\mu K^2$]",fontsize=20)
         plt.tick_params(labelsize=20)
     
     def plot_fg2(self):
-        l = np.arange(self.lmax+1)
-        _,fg1_nl_e,fg1_nl_b = self.sim_fg1.noise_spectra(500)
-        _,fg2_nl_e,fg2_nl_b = self.sim_fg2.noise_spectra(500)
-        _,fg1_res_e,fg1_res_b = self.sim_fg1.fg_res_mean(500)
-        _,fg2_res_e,fg2_res_b = self.sim_fg2.fg_res_mean(500)
-        fg1_rnl_b = fg1_nl_b + fg1_res_b
-        fg2_rnl_b = fg2_nl_b + fg2_res_b
-        fg1_rnl_e = fg1_nl_e + fg1_res_e
-        fg2_rnl_e = fg2_nl_e + fg2_res_e
-        lfac = (l*(l+1))/(2*np.pi)
+        fname = '../Data/paper/simFG2.pkl'
+        if not os.path.isfile(fname):
+            data = {}
+            _,fg1_nl_e,fg1_nl_b = self.sim_fg1.noise_spectra(500)
+            _,fg2_nl_e,fg2_nl_b = self.sim_fg2.noise_spectra(500)
+            _,fg1_res_e,fg1_res_b = self.sim_fg1.fg_res_mean(500)
+            _,fg2_res_e,fg2_res_b = self.sim_fg2.fg_res_mean(500)
+            data['fg1_rnl_b'] = fg1_nl_b + fg1_res_b
+            data['fg2_rnl_b'] = fg2_nl_b + fg2_res_b
+            data['fg1_rnl_e'] = fg1_nl_e + fg1_res_e
+            data['fg2_rnl_e'] = fg2_nl_e + fg2_res_e
+            pl.dump(data,open(fname,'wb'))
+            print('Data Saved to file')
+        else:
+            data = pl.load(open(fname,'rb'))
+            print('Data Loaded from file')
+
+
+        fg1_rnl_b = data['fg1_rnl_b']
+        fg2_rnl_b = data['fg2_rnl_b']
+        fg1_rnl_e = data['fg1_rnl_e']
+        fg2_rnl_e = data['fg2_rnl_e']
+
         plt.figure(figsize=(16, 8))
         fig, (ax1, ax2)  = plt.subplots(1, 2,figsize=(16, 8))
 
@@ -74,14 +87,16 @@ class simStat:
         ax1.loglog(fg2_rnl_e*self.sim_fg1.Tcmb**2 ,label=f"{self.fg2}",c='g',lw=2)
         ax1.legend(fontsize=15)
         ax1.set_xlabel("$\ell$",fontsize=20)
-        ax1.set_ylabel("$\\frac{1}{b_\ell^2}\\left(C_\ell^{FG\;res} + N_\ell \\right)$ [$\mu K^2$]",fontsize=20)
+        ax1.set_ylabel("$\\frac{1}{b_\ell^2}\\left(C_\ell^{fg_{res}} + N_\ell \\right)$ [$\mu K^2$]",fontsize=20)
         ax1.set_xlim(2,600)
+        ax1.set_ylim(1e-6,1e-1)
 
         ax2.loglog(self.sim_fg1.cl_len[2,:]*self.sim_fg1.Tcmb**2 ,c='k',lw=2,label="Fiducial BB")
         ax2.loglog(fg1_rnl_b*self.sim_fg1.Tcmb**2 ,label=f"{self.fg1}",c='b',lw=2)
         ax2.loglog(fg2_rnl_b*self.sim_fg1.Tcmb**2 ,label=f"{self.fg2}",c='magenta',lw=2)
         ax2.legend(fontsize=15)
         ax2.set_xlabel("$\ell$",fontsize=20)
+        ax2.set_ylim(1e-7,1e-2)
         ax2.set_xlim(2,600)
         
 
@@ -111,33 +126,57 @@ class recStat:
         """
         Plot the impact of foregrounds on the reconstruction
         """
-        rec_nofg = self.rec_nofg
-        rec_fg1 = self.rec_fg1
-        rec_fg2 = self.rec_fg2
 
-        nofg_cl = rec_nofg.get_qcl_wR_stat(n=400,n1=True,rdn0=True)
-        fg2_cl = rec_fg2.get_qcl_wR_stat(n=400,n1=True,rdn0=True)
-        fid = rec_fg1.bin_cell(rec_fg1.cl_pp*rec_fg1.Lfac)
+        fname = '../Data/paper/recFG.pkl'
+        if os.path.isfile(fname):
+            data = pl.load(open(fname,'rb'))
+            print('Data Loaded from file')
+        else:
+            rec_nofg = self.rec_nofg
+            rec_fg1 = self.rec_fg1
+            rec_fg2 = self.rec_fg2
+            data = {}
+            data['nofg_cl'] = rec_nofg.get_qcl_wR_stat(n=400,n1=True,rdn0=True)
+            data['fg2_cl'] = rec_fg2.get_qcl_wR_stat(n=400,n1=True,rdn0=True)
+            data['fid'] = rec_fg1.bin_cell(rec_fg1.cl_pp*rec_fg1.Lfac)
+            data['fidm'] = rec_fg1.cl_pp*rec_fg1.Lfac
+            data['NOFG-MCN0'] = rec_nofg.Lfac*(rec_nofg.MCN0()/rec_nofg.response_mean()**2 )
+            data['fg1-MCN0'] = rec_fg1.Lfac*(rec_fg1.MCN0()/rec_fg1.response_mean()**2 )
+            data['fg2-MCN0'] = rec_fg2.Lfac*(rec_fg2.MCN0()/rec_fg2.response_mean()**2 )
+            data['NOFG-MCER'] = rec_fg1.Lfac*(rec_nofg.MCN0()/rec_nofg.response_mean()**2 )/100
+            data['fg2-MCER'] = rec_fg1.Lfac*(rec_fg2.MCN0()/rec_fg2.response_mean()**2 )/100
+            data['NOFG-MF'] = rec_nofg.Lfac*rec_nofg.mean_field_cl()
+            data['fg2-MF'] = rec_fg2.Lfac*rec_fg2.mean_field_cl()
+            data['B'] = rec_fg1.B
+            pl.dump(data,open(fname,'wb'))
+            print('Data Saved to file')
+
+
+
+
+        nofg_cl = data['nofg_cl']
+        fg2_cl = data['fg2_cl']
+        fid = data['fid']
 
         fig, axs = plt.subplots(2, 1,figsize=(9,9), gridspec_kw={'height_ratios': [4, 1]}, sharex=True)
         plt.setp(axs, xlim=(2,620))
         fig.subplots_adjust(hspace=0)
 
 
-        axs[0].semilogy(rec_fg1.cl_pp*rec_fg1.Lfac,label='Fiducial',c='grey',lw=2)
-        axs[0].semilogy(rec_nofg.Lfac*(rec_nofg.MCN0()/rec_nofg.response_mean()**2 ),label='NOFG-MCN0',c='b')
-        axs[0].semilogy(rec_fg1.Lfac*(rec_fg1.MCN0()/rec_fg1.response_mean()**2 ),label=f'{self.fg1}-MCN0',c='r',)
-        axs[0].semilogy(rec_fg2.Lfac*(rec_fg2.MCN0()/rec_fg2.response_mean()**2 ),label=f'{self.fg2}-MCN0',)
-        axs[0].semilogy(rec_fg1.Lfac*(rec_nofg.MCN0()/rec_nofg.response_mean()**2 )/100,label='NOFG-MC Error')
-        axs[0].semilogy(rec_fg1.Lfac*(rec_fg2.MCN0()/rec_fg2.response_mean()**2 )/100,label=f'{self.fg2} MC-Error')
-        axs[0].semilogy(rec_nofg.Lfac*rec_nofg.mean_field_cl(),label='NOFG-MF',c='y')
-        axs[0].semilogy(rec_fg2.Lfac*rec_fg2.mean_field_cl(),label=f'{self.fg2}-MF',c='g')
+        axs[0].semilogy(data['fidm'],label='Fiducial',c='grey',lw=2)
+        axs[0].semilogy(data['NOFG-MCN0'],label='NOFG-MCN0',c='b')
+        axs[0].semilogy(data['fg1-MCN0'],label=f'{self.fg1}-MCN0',c='r',)
+        axs[0].semilogy(data['fg2-MCN0'],label=f'{self.fg2}-MCN0',)
+        axs[0].semilogy(data['NOFG-MCER'],label='NOFG-MC Error')
+        axs[0].semilogy(data['fg2-MCER'],label=f'{self.fg2} MC-Error')
+        axs[0].semilogy(data['NOFG-MF'],label='NOFG-MF',c='y')
+        axs[0].semilogy(data['fg2-MF'],label=f'{self.fg2}-MF',c='g')
         axs[0].legend(ncol=2, fontsize=15)
         axs[0].set_ylim(1e-9,1e-5)
         axs[0].set_ylabel('$L^2 (L + 1)^2 C_L^{\phi\phi}$',fontsize=20)
 
-        axs[1].errorbar(rec_fg1.B,nofg_cl.mean(axis=0)/fid,yerr=nofg_cl.std(axis=0)/fid,label='NOFG',c='b',fmt='o')
-        axs[1].errorbar(rec_fg1.B,fg2_cl.mean(axis=0)/fid,yerr=fg2_cl.std(axis=0)/fid,label=f'{self.fg2}',c='r',fmt='o')
+        axs[1].errorbar(data['B'],nofg_cl.mean(axis=0)/fid,yerr=nofg_cl.std(axis=0)/fid,label='NOFG',c='b',fmt='o')
+        axs[1].errorbar(data['B'],fg2_cl.mean(axis=0)/fid,yerr=fg2_cl.std(axis=0)/fid,label=f'{self.fg2}',c='r',fmt='o')
         axs[1].set_ylim(-0.1,2.3)
         axs[1].legend(ncol=2, fontsize=15,loc='upper left')
         axs[1].axhline(1,c='k')
