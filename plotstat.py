05cb20c37e1d51189161dde9673d73c17c699d26
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-11-02 10:57:34 -0700

    cleanup
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- plotstat.py
+++ plotstat.py
@@ -10,13 +10,14 @@ import matplotlib.gridspec as gridspec
 import emcee
 from scipy.stats import gaussian_kde
 
-datapath = '../Data/paperNew'
+datapath = '../Data/PlotData'
 plotpath = '../Notebooks/plots'
 
 os.makedirs(datapath,exist_ok=True)
 os.makedirs(plotpath,exist_ok=True)
 
 if socket.gethostname() == 'vmi401751.contaboserver.net':
+    uselatex = True
     plt.rcParams['text.usetex']=True
     plt.rcParams['ytick.minor.visible'] =True
     plt.rcParams['xtick.minor.visible'] = True
@@ -47,6 +48,8 @@ if socket.gethostname() == 'vmi401751.contaboserver.net':
     plt.rcParams['xtick.minor.pad']='10'
     plt.rcParams['hatch.color'] = 'black'
     plt.rcParams['lines.dashed_pattern']=3, 1.5
+else:
+    uselatex = False
     
 def find_density(arr):
     density = gaussian_kde(arr,.3)
@@ -56,6 +59,13 @@ def find_density(arr):
 
 
 class Alens_fit:
+    """
+    Class to fit for the Alens parameter
+
+    Parameters
+    ----------
+    reco : object : Reconstruction object
+    """
     
     def __init__(self,reco):
         self.rec = reco
@@ -64,25 +74,43 @@ class Alens_fit:
         self.icov = self.icov_()
         
     def spectra_(self):
+        """
+        Get the spectra to fit for
+        """
         return self.rec.get_qcl_wR_stat(400,rdn0=True,n1=True).mean(axis=0)
     
     def icov_(self):
+        """
+        Get the inverse covariance matrix
+        """
         cov = np.cov(self.rec.get_qcl_wR_stat(400,rdn0=True,n1=True).T)
         return np.linalg.inv(cov)
     
     def chi_sq(self,alens):
+        """
+        Get the Chi^2
+        """
         dcl = self.spectra - (alens*self.fid)
         return np.dot(dcl,np.dot(self.icov,dcl))
     
     def log_prior(self,theta):
+        """
+        Get the log prior
+        """
         if 0.5 < theta < 1.5:
             return 0.0
         return -np.inf
 
     def log_likelihood(self,theta):
+        """
+        Get the log likelihood
+        """
         return -0.5 * self.chi_sq(theta)
 
     def log_probability(self,theta):
+        """
+        Get the log probability
+        """
         lp = self.log_prior(theta)
         if not np.isfinite(lp):
             return -np.inf
@@ -90,6 +118,9 @@ class Alens_fit:
     
 
     def get_samples(self):
+        """
+        Samples from the posterior
+        """
         pos = [1] + 1e-1 * np.random.randn(64, 1)
         nwalkers,ndim = pos.shape
         sampler = emcee.EnsembleSampler(nwalkers,ndim,self.log_probability)
@@ -98,6 +129,10 @@ class Alens_fit:
         return flat_samples
 
 class crazymix:
+    """
+    Class to fit for the Alens parameter
+    with mismatched simulations
+    """
 
     def __init__(self,s1d1,other,idx=0,do_MC=False):
         self.s1d1 = s1d1
@@ -154,7 +189,12 @@ class crazymix:
         sampler.run_mcmc(pos, 4000, progress=True)
         flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
         return flat_samples
+    
+
 class Planck:
+    """
+    Class for Planck data
+    """
 
     def __init__(self):
         datadir = '../Data/planck/'
@@ -334,9 +374,9 @@ class recStat:
         fig.subplots_adjust(hspace=0)
 
         axs[0].semilogy(data['fidm'],label='Signal',c='grey',lw=2)
-        axs[0].semilogy(data['NOFG-MCN0'], c='g', label="$\\textbf{No FG}$",lw=2)
-        axs[0].semilogy(data['fg1-MCN0'], c='b', label="$\\textbf{s0d0}$",lw=2)
-        axs[0].semilogy(data['fg2-MCN0'], c='r', label="$\\textbf{s1d1}$",lw=2)
+        axs[0].semilogy(data['NOFG-MCN0'], c='g', label="$\\textbf{No FG}$" if uselatex else "No FG",lw=2)
+        axs[0].semilogy(data['fg1-MCN0'], c='b', label="$\\textbf{s0d0}$" if uselatex else "s0d0",lw=2)
+        axs[0].semilogy(data['fg2-MCN0'], c='r', label="$\\textbf{s1d1}$" if uselatex else "s1d1",lw=2)
 
         axs[0].semilogy(data['NOFG-MCER']+(data['fidm']/100),c='g',ls=':',lw=3)
         axs[0].semilogy(data['fg2-MCER']+(data['fidm']/100),c='r',ls=':',lw=3)
@@ -420,11 +460,11 @@ class recStat:
         plt.figure(figsize=(15,15))
         ax = plt.subplot(gs[0, 0])
         hp.gnomview(inputk,reso=res,rot=[r1,r2],norm='hist',title='',xsize=xsize,notext=True,hold=True)
-        plt.title('$\kappa_{LM}^\\texttt{Input}$',fontsize=20)
+        plt.title('$\kappa_{LM}^\\texttt{Input}$' if uselatex else '$\kappa_{LM}^{Input}$',fontsize=20)
         #plt.text(-.16,-.135,"$Resolution = 5^\prime/pixel, 200\\times200\;pixel$",rotation=90,fontsize=18)
         ax = plt.subplot(gs[0, 1])
         hp.gnomview(output,reso=res,rot=[r1,r2],norm='hist',xsize=xsize,title='Output',notext=True,hold=True)
-        plt.title('$\kappa_{LM}^\\texttt{Output}$',fontsize=18)
+        plt.title('$\kappa_{LM}^\\texttt{Output}$' if uselatex else '$\kappa_{LM}^{Output}$',fontsize=18)
         #ax = plt.subplot(gs[0, 2])
         #hp.gnomview(inputk-output,reso=res,rot=[r1,r2],norm='hist',xsize=xsize,title='Output',notext=True,hold=True)
         #plt.title('$\kappa_{LM}^\\texttt{Input-Output}$',fontsize=18)
@@ -459,7 +499,10 @@ class recStat:
         SNR_fg2 = data['fg2-SNR']
         SNR_fg3 = data['fg3-SNR']
         #cases = ['Planck(pol)','Planck(MV)','No FG', 's0d0', 's1d1']
-        cases = ['$\\textbf{No FG}$', '$\\textbf{s0d0}$','$\\textbf{s1d1}$',"$\\textbf{s1d1}$ \n ($f_{sky}=0.9$)",'$\\textsc{Planck}$'+"\n $(Pol)$"]#,'Planck(MV)']
+        if uselatex:
+            cases = ['$\\textbf{No FG}$', '$\\textbf{s0d0}$','$\\textbf{s1d1}$',"$\\textbf{s1d1}$ \n ($f_{sky}=0.9$)",'$\\textsc{Planck}$'+"\n $(Pol)$"]#,'Planck(MV)']
+        else:
+            cases = ['No FG', 's0d0', 's1d1',"$s1d1$ \n ($f_{sky}=0.9$)",'Planck'+"\n $(Pol)$"]
         pl_pol = 9
         pl_mv = 40
         pl_pol_npipe = pl_pol*.2+pl_pol
@@ -501,11 +544,11 @@ class recStat:
         
         stat = data['stat']
         plt.figure(figsize=(8,7))
-        plt.loglog(data['fid'],label='$\\texttt{Signal}$',c='grey',lw=2)
+        plt.loglog(data['fid'],label='$\\texttt{Signal}$' if uselatex else 'Signal',c='grey',lw=2)
         plt.loglog(data['mcn0'],label='$N_L^{(0)}$',c='r')
         plt.loglog(data['mcn1'],label='$N_L^{(1)}$',c='g')
         plt.loglog(data['mf'],label='$C_L^{MF}$',c='b')
-        plt.errorbar(data['B'],stat.mean(axis=0),yerr=stat.std(axis=0),fmt='o',c='k',ms=6,capsize=2,label='$\\texttt{Reconstructed}$')
+        plt.errorbar(data['B'],stat.mean(axis=0),yerr=stat.std(axis=0),fmt='o',c='k',ms=6,capsize=2,label='$\\texttt{Reconstructed}$' if uselatex else 'Reconstructed')
         plt.xlim(2,600)
         plt.legend(ncol=2, fontsize=20)
         plt.xlabel('$L$',fontsize=25)
@@ -555,8 +598,8 @@ class recStat:
         data = pl.load(open(fname,'rb'))
         
         plt.figure(figsize=(7,7))
-        plt.loglog((Planck().PP['N'])[:len(data['mcn0'])],label='$\\textsc{Planck}\\texttt{(Pol)}$',lw=3)
-        plt.loglog(data['mcn0'],label='$\\textsc{LiteBIRD}\\texttt{(EB)}$',c='r',lw=3)
+        plt.loglog((Planck().PP['N'])[:len(data['mcn0'])],label='$\\textsc{Planck}\\texttt{(Pol)}$' if uselatex else "$Planck(Pol)$" ,lw=3)
+        plt.loglog(data['mcn0'],label='$\\textsc{LiteBIRD}\\texttt{(EB)}$' if uselatex else "$LiteBIRD(EB)$",c='r',lw=3)
         plt.loglog(data['fid'],label='Signal',c='grey',lw=3)
         plt.xlim(2,600)
         plt.ylabel('$\\frac{L^2 (L + 1)^2}{2\pi} N_L^{(0),MC}$',fontsize=25)
@@ -588,7 +631,10 @@ class recStat:
         fg1_samp = data['fg1_samp']
         fg2_samp = data['fg2_samp']
         data_to_plot = [nofg_samp, fg1_samp, fg2_samp]
-        labels = ['$\\textbf{No FG}$', '$\\textbf{s0d0}$', '$\\textbf{s1d1}$']
+        if uselatex:
+            labels = ['$\\textbf{No FG}$', '$\\textbf{s0d0}$', '$\\textbf{s1d1}$']
+        else:
+            labels = ['No FG', 's0d0', 's1d1']
 
         plt.figure(figsize=(6, 6))
         sns.boxplot(data=data_to_plot)
@@ -608,11 +654,11 @@ class recStat:
         s1d1 = pvalue['s1d1']
         B = pvalue['b']
         plt.figure(figsize=(6,6))
-        plt.plot(B,s0d0,label='$\\textbf{s0d0}$',marker='o',c='C10')
-        plt.plot(B,s1d1,label='$\\textbf{s1d1}$',marker='o',c='C3')
+        plt.plot(B,s0d0,label='$\\textbf{s0d0}$' if uselatex else "s0d0",marker='o',c='C10')
+        plt.plot(B,s1d1,label='$\\textbf{s1d1}$' if uselatex else "s1d1",marker='o',c='C3')
         plt.axhline(0.05,color='k',ls='--',label='$0.05$',lw=3)
         plt.legend(fontsize=15)
-        plt.ylabel('$\\mathrm{KS} \;\; p-\\mathrm{value}$',fontsize=25)
+        plt.ylabel('$\\mathrm{KS} \;\; p-\\mathrm{value}$' if uselatex else "KS p-value",fontsize=25)
         plt.xlabel('$L$',fontsize=25)
         plt.xticks(fontsize=25)
         plt.yticks(fontsize=25)
@@ -644,9 +690,9 @@ class recStat:
         smm3=np.mean(sfg2)
         
         plt.figure(figsize=(8,6))
-        plt.plot(*find_density(snofg),lw=3,label='$\\textbf{No FG}$')
-        plt.plot(*find_density(sfg1),lw=3,label='$\\textbf{s0d0}$')
-        plt.plot(*find_density(sfg2),lw=3,label='$\\textbf{s1d1}$')
+        plt.plot(*find_density(snofg),lw=3,label='$\\textbf{No FG}$'if uselatex else "No FG")
+        plt.plot(*find_density(sfg1),lw=3,label='$\\textbf{s0d0}$' if uselatex else "s0d0")
+        plt.plot(*find_density(sfg2),lw=3,label='$\\textbf{s1d1}$' if uselatex else "s1d1")
         plt.text(smm1-.013,1.02,f"{smm1:.2f}",fontsize=25)
         plt.text(smm2-.013,1.02,f"{smm2:.2f}",fontsize=25)
         plt.text(smm3-.013,1.02,f"{smm3:.2f}",fontsize=25)
